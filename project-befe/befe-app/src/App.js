7955e37501926a3f68facc32554d3e1742606938
Perapat klaysamneang <klaysamneang_p@su.ac.th>
2025-11-13 06:40:40 +0000

    prepare2

--- project-befe/befe-app/src/App.js
+++ project-befe/befe-app/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+
+// Pages
+import HomePage from './pages/HomePage';
+import BookListPage from './pages/BookListPage';
+import BookDetailPage from './pages/BookDetailPage';
+import CategoryPage from './pages/CategoryPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import LoginPage from './pages/LoginPage';
+import AddBookPage from './pages/AddBookPage';
+import BooklistAdmin from './pages/BooklistAdmin';
+function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow bg-gray-50">
+        <Outlet /> {/* แทนที่ Routes ซ้อน */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* ✅ Routes ที่ไม่มี Navbar/Footer */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/store-manager/add-book" element={<AddBookPage />} />
+        <Route path='/store-manager/booklist' element={<BooklistAdmin/>}/>
+        {/* ✅ Routes ที่มี Navbar/Footer */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/books" element={<BookListPage />} />
+          <Route path="/books/:id" element={<BookDetailPage />} />
+          <Route path="/categories" element={<CategoryPage />} />
+          <Route path="/categories/:category" element={<CategoryPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
